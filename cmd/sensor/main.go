b332249cde8d580dbff309d1edc45b66109c9f5f
Titouan Cocheril <titouan.cocheril25@gmail.com>
2024-01-07 13:07:18 +0100

    feat: add config sensor file

--- cmd/sensor/main.go
+++ cmd/sensor/main.go
@@ -3,34 +3,55 @@ package main
 import (
 	"Airport_MQTT/api"
 	"Airport_MQTT/pkg/sensor"
+	"encoding/json"
 	"fmt"
 	"github.com/krisukox/google-flights-api/iata"
+	"io/ioutil"
 	"time"
 )
 
+type Config struct {
+	Sensors    []sensor.SensorData `json:"sensors"`
+	MQTTConfig sensor.SensorConfig `json:"mqttConfig"`
+}
+
 func main() {
-	iataCode := "CDG"
-
-	location := iata.IATATimeZone(iataCode)
-	if location.City == "" {
-		fmt.Println("Code IATA non supporté :", iataCode)
-		return
-	}
-
-	fmt.Printf("Aéroport : %s\n", location.City)
-
-	weatherData, err := api.FetchWeatherData(location.City)
+	configFile, err := ioutil.ReadFile("pkg/sensor/config.json")
 	if err != nil {
-		fmt.Println("Erreur lors de la récupération des données météo:", err)
+		fmt.Println("Erreur lors de la lecture du fichier de configuration:", err)
 		return
 	}
 
-	fmt.Printf("Météo pour %s: Température %d°C, Vitesse du vent %d km/h, Humidité %d%%\n",
-		location.City, weatherData.Current.Temperature, weatherData.Current.WindSpeed, weatherData.Current.Humidity)
+	var config Config
+	if err := json.Unmarshal(configFile, &config); err != nil {
+		fmt.Println("Erreur lors du parsing du fichier de configuration:", err)
+		return
+	}
 
-	sensorData := sensor.NewSensorData(123, iataCode, "Temperature", float64(weatherData.Current.Temperature), time.Now())
-	sensorConfig := sensor.NewSensorConfig("mqtt.com", 1883, 0, 123)
+	for _, s := range config.Sensors {
+		iataCode := s.AirportCode
 
-	sensorData.Display()
-	sensorConfig.Display()
+		fmt.Println("test", iataCode)
+		location := iata.IATATimeZone(iataCode)
+		if location.City == "" {
+			fmt.Println("Code IATA non supporté :", iataCode)
+			continue
+		}
+
+		weatherData, err := api.FetchWeatherData(location.City)
+		if err != nil {
+			fmt.Println("Erreur lors de la récupération des données météo:", err)
+			continue
+		}
+
+		fmt.Printf("Météo pour %s: Température %d°C, Vitesse du vent %d km/h, Humidité %d%%\n",
+			location.City, weatherData.Current.Temperature, weatherData.Current.WindSpeed, weatherData.Current.Humidity)
+
+		s.Value = float64(weatherData.Current.Temperature)
+		s.Timestamp = time.Now()
+
+		s.Display()
+	}
+
+	config.MQTTConfig.Display()
 }
