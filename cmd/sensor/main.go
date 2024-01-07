654b5a56767f72050cdb6cc26e5aa2474b620684
Titouan Cocheril <titouan.cocheril25@gmail.com>
2024-01-07 23:49:04 +0100

    feat: get data with frequency

--- cmd/sensor/main.go
+++ cmd/sensor/main.go
@@ -1,57 +1,42 @@
 package main
 
 import (
-	"Airport_MQTT/api"
-	"Airport_MQTT/pkg/sensor"
+	sensor2 "Airport_MQTT/internal/sensor"
 	"encoding/json"
 	"fmt"
-	"github.com/krisukox/google-flights-api/iata"
-	"io/ioutil"
-	"time"
+	"os"
 )
 
-type Config struct {
-	Sensors    []sensor.SensorData `json:"sensors"`
-	MQTTConfig sensor.SensorConfig `json:"mqttConfig"`
+type SensorData struct {
+	Sensors    []sensor2.Sensor `json:"sensors"`
+	MQTTConfig sensor2.Config   `json:"mqttConfig"`
 }
 
 func main() {
-	configFile, err := ioutil.ReadFile("pkg/sensor/config.json")
+	configFile, err := os.ReadFile("internal/sensor/data.json")
 	if err != nil {
-		fmt.Println("Erreur lors de la lecture du fichier de configuration:", err)
+		fmt.Println("Erreur lors de la lecture du fichier de sensor:", err)
 		return
 	}
 
-	var config Config
-	if err := json.Unmarshal(configFile, &config); err != nil {
-		fmt.Println("Erreur lors du parsing du fichier de configuration:", err)
+	var sensorData SensorData
+	if err := json.Unmarshal(configFile, &sensorData); err != nil {
+		fmt.Println("Erreur lors du parsing du fichier de données sensor:", err)
 		return
 	}
 
-	for _, s := range config.Sensors {
-		iataCode := s.AirportCode
+	for _, sc := range sensorData.Sensors {
+		go func(sc sensor2.Sensor) {
+			dataChannel := make(chan float64)
 
-		fmt.Println("test", iataCode)
-		location := iata.IATATimeZone(iataCode)
-		if location.City == "" {
-			fmt.Println("Code IATA non supporté :", iataCode)
-			continue
-		}
+			go sensor2.StartCaptureData(sc.AirportCode, sc.Measurement, sc.Frequency, dataChannel)
 
-		weatherData, err := api.FetchWeatherData(location.City)
-		if err != nil {
-			fmt.Println("Erreur lors de la récupération des données météo:", err)
-			continue
-		}
-
-		fmt.Printf("Météo pour %s: Température %d°C, Vitesse du vent %d km/h, Humidité %d%%\n",
-			location.City, weatherData.Current.Temperature, weatherData.Current.WindSpeed, weatherData.Current.Humidity)
-
-		s.Value = float64(weatherData.Current.Temperature)
-		s.Timestamp = time.Now()
-
-		s.Display()
+			for data := range dataChannel {
+				fmt.Printf("%s: %f\n", sc.Measurement, data)
+			}
+		}(sc)
 	}
 
-	config.MQTTConfig.Display()
+	fmt.Println("Appuyez sur 'Enter' pour arrêter.")
+	fmt.Scanln()
 }
--- internal/sensor/config.go
+++ internal/sensor/config.go
@@ -1,26 +1,8 @@
 package sensor
 
-import "fmt"
-
 type Config struct {
 	BrokerAddress string
 	BrokerPort    int
 	QoS           byte
 	ClientID      int
-}
-
-func NewConfig(brokerAddress string, brokerPort int, qos byte, clientID int) *Config {
-	return &Config{
-		BrokerAddress: brokerAddress,
-		BrokerPort:    brokerPort,
-		QoS:           qos,
-		ClientID:      clientID,
-	}
-}
-
-func (sc *Config) Display() {
-	fmt.Printf("Broker Address: %s\n", sc.BrokerAddress)
-	fmt.Printf("Broker Port: %d\n", sc.BrokerPort)
-	fmt.Printf("QoS: %d\n", sc.QoS)
-	fmt.Printf("Client ID: %s\n", sc.ClientID)
 }
