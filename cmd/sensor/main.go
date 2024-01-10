3838d78bf1643745efd26712eb577befbab77600
Titouan Cocheril <titouan.cocheril25@gmail.com>
2024-01-10 19:04:44 +0100

    feat: generate sensor uuid

--- cmd/sensor/main.go
+++ cmd/sensor/main.go
@@ -2,25 +2,37 @@ package main
 
 import (
 	"Airport_MQTT/internal/sensor"
+	"flag"
+	"fmt"
+	"github.com/google/uuid"
 	"os"
 	"strconv"
 )
 
 func main() {
-	if len(os.Args) < 5 {
-		panic("Usage: main <sensorId> <iataCode> <measurement> <frequency>")
+	sensorIdFlag := flag.String("sensorId", "", "ID of the sensor")
+	iataCodeFlag := flag.String("iataCode", "", "IATA code")
+	measurementFlag := flag.String("measurement", "", "Type of measurement")
+	frequencyStrFlag := flag.String("frequency", "", "Frequency of measurement")
+
+	flag.Parse()
+
+	if _, err := uuid.Parse(*sensorIdFlag); err != nil || *sensorIdFlag == "" {
+		*sensorIdFlag = uuid.New().String()
+		fmt.Println("SensorId was not an uuid new one is :", *sensorIdFlag)
 	}
 
-	sensorId := os.Args[1]
-	iataCode := os.Args[2]
-	measurement := os.Args[3]
-	frequencyStr := os.Args[4]
-
-	frequency, err := strconv.Atoi(frequencyStr)
+	frequency, err := strconv.Atoi(*frequencyStrFlag)
 	if err != nil {
 		panic("Frequency must be an integer")
 	}
 
-	mySensor := sensor.NewSensor(nil, sensorId, iataCode, measurement, frequency)
+	if *iataCodeFlag == "" || *measurementFlag == "" {
+		fmt.Println("Missing required flags")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	mySensor := sensor.NewSensor(nil, *sensorIdFlag, *iataCodeFlag, *measurementFlag, frequency)
 	mySensor.StartSensor()
 }
