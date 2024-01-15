aacdb2f2a066b1191eb39bb67d0da1acc062d8b8
Elias Morio <elias.morio@icloud.com>
2024-01-15 20:00:03 +0100

    refactor: refactor and remove unnecessary codes  in Sensor Data Repository

--- internal/persist/sensor_data_repository.go
+++ internal/persist/sensor_data_repository.go
@@ -3,9 +3,7 @@ package persist
 import (
 	"Airport_MQTT/internal/model"
 	"context"
-	"fmt"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"time"
 )
@@ -14,18 +12,6 @@ const (
 	DATABASE   = "airport_sensors"
 	COLLECTION = "sensor_data"
 )
-
-type SensorDataMongo struct {
-	Timestamp primitive.DateTime
-	Value     float32
-	Metadata  SensorDataMongoMetadata
-}
-
-type SensorDataMongoMetadata struct {
-	SensorId    string
-	AirportIATA string
-	Nature      model.Nature
-}
 
 type Filter struct {
 	SensorId    string
@@ -37,7 +23,6 @@ type Filter struct {
 
 type SensorDataRepository interface {
 	Store(sensor model.SensorData) error
-	FindAllReadingsByFilter(filter Filter) ([]model.Sensor, error)
 }
 
 type SensorDataMongoRepository struct {
@@ -49,90 +34,23 @@ func NewSensorDataRepository() SensorDataRepository {
 	return &SensorDataMongoRepository{client: client}
 }
 
+func toBson(sensorData model.SensorData) bson.D {
+	return bson.D{
+		{"timestamp", sensorData.Timestamp},
+		{"value", sensorData.Value},
+		{"metadata", bson.D{
+			{"sensorId", sensorData.SensorId},
+			{"airportIATA", sensorData.AirportIATA},
+			{"sensorType", sensorData.Nature},
+		}},
+	}
+}
+
 func (r *SensorDataMongoRepository) Store(sensor model.SensorData) error {
 	coll := r.client.Database(DATABASE).Collection(COLLECTION)
-	_, err := coll.InsertOne(context.Background(), bson.D{
-		{"timestamp", sensor.Timestamp},
-		{"value", sensor.Value},
-		{"metadata", bson.D{
-			{"sensorId", sensor.SensorId},
-			{"airportIATA", sensor.AirportIATA},
-			{"sensorType", sensor.Nature},
-		}},
-	})
+	_, err := coll.InsertOne(context.Background(), toBson(sensor))
 	if err != nil {
 		return err
 	}
 	return nil
-}
-
-func (r *SensorDataMongoRepository) FindAllReadingsByFilter(filter Filter) ([]model.Sensor, error) {
-	coll := r.client.Database(DATABASE).Collection(COLLECTION)
-
-	//prepare request filter
-	mongoFilter := map[string]interface{}{}
-	if filter.SensorId != "" {
-		mongoFilter["metadata.sensorId"] = filter.SensorId
-	}
-	if filter.AirportIATA != "" {
-		mongoFilter["metadata.airportIATA"] = filter.AirportIATA
-	}
-	if filter.Type != 0 {
-		mongoFilter["metadata.sensorType"] = filter.Type
-	}
-	if !filter.From.IsZero() && !filter.To.IsZero() {
-		mongoFilter["timestamp"] = map[string]interface{}{
-			"$gte": filter.From,
-			"$lte": filter.To,
-		}
-	}
-
-	//execute request
-	cursor, err := coll.Find(context.Background(), mongoFilter)
-	if err != nil {
-		return nil, err
-	}
-
-	sensors := mapSensorDataMongo(cursor)
-
-	return sensors, nil
-}
-
-func mapSensorDataMongo(sensorData *mongo.Cursor) []model.Sensor {
-	var sensors []model.Sensor
-	for sensorData.Next(context.Background()) {
-		var sensorDataMongo SensorDataMongo
-		err := sensorData.Decode(&sensorDataMongo)
-		if err != nil {
-			fmt.Println("Error while decoding sensor data")
-			continue
-		}
-
-		sensor := model.Sensor{
-			SensorId:    sensorDataMongo.Metadata.SensorId,
-			AirportIATA: sensorDataMongo.Metadata.AirportIATA,
-			Type:        sensorDataMongo.Metadata.Nature,
-		}
-
-		reading := model.Reading{
-			Timestamp: sensorDataMongo.Timestamp.Time(),
-			Value:     sensorDataMongo.Value,
-		}
-
-		//check if sensor already exists
-		exists := false
-		for i, s := range sensors {
-			if s.SensorId == sensor.SensorId {
-				sensors[i].Readings = append(sensors[i].Readings, reading)
-				exists = true
-				break
-			}
-		}
-
-		if !exists {
-			sensor.Readings = []model.Reading{reading}
-			sensors = append(sensors, sensor)
-		}
-	}
-	return sensors
 }
