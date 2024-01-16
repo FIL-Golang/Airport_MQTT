d12942f8b39a09ac7f259b29ebf9de1d533319dc
Titouan Cocheril <titouan.cocheril25@gmail.com>
2024-01-16 21:25:23 +0100

    feat: mongo connexion api methods

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,44 +1,29 @@
-package api
+package main
 
 import (
-	"Airport_MQTT/internal/persist"
-	"encoding/json"
+	"Airport_MQTT/internal/api"
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
-type Person struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
-}
+func main() {
+	router := mux.NewRouter()
 
-func test(w http.ResponseWriter, r *http.Request) {
-	test := Person{ID: 1, Name: "allo"}
-	JsonData, err := json.Marshal(test)
+	http.Handle("/", router)
 
+	router.HandleFunc("/sendData", api.SendDataExample)
+	router.HandleFunc("/getSensor", api.GetSensor).
+		Queries("sensorID", "{sensorID}", "airportIATA", "{airportIATA}", "type", "{type}")
+	router.HandleFunc("/getReadings", api.GetReadings).
+		Queries("sensorID", "{sensorID}", "airportIATA", "{airportIATA}", "type", "{type}")
+	router.HandleFunc("/globalDailyAverage", api.GlobalDailyAverage)
+	router.HandleFunc("/onTimeList", api.OnTimeList).Queries("type", "{type}")
+
+	fmt.Printf("Serveur écoutant sur le port %d...\n", 8080)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", 8080), nil)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		fmt.Printf("Erreur : %v\n", err)
 		return
 	}
-
-	// Configuration de l'en-tête Content-Type pour indiquer que la réponse est au format JSON
-	w.Header().Set("Content-Type", "application/json")
-
-	//renvoi des données
-	w.Write(JsonData)
-}
-
-func GlobalDailyAverage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Bonjour depuis GlobalDailyAverage!")
-}
-
-func DailyAverage(w http.ResponseWriter, r *http.Request) {
-	typeParam := r.URL.Query().Get("type")
-	fmt.Fprint(w, typeParam)
-	persist.NewSensorDataRepository().GetAvg()
-}
-
-func OnTimeList(w http.ResponseWriter, r *http.Request) {
-	typeParam := r.URL.Query().Get("type")
-	fmt.Fprint(w, typeParam)
 }
