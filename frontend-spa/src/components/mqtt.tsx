dc17d7113f4473bf9d3b761bcd6a0fdee312d072
Titouan Cocheril <titouan.cocheril25@gmail.com>
2024-01-17 00:03:08 +0100

    feat: websocket alert

--- frontend-spa/src/components/mqtt.tsx
+++ frontend-spa/src/components/mqtt.tsx
@@ -1,15 +1,63 @@
-import useMQTT from '../hook/useMqtt.tsx';
+import { useEffect } from 'react';
+import mqtt, { MqttClient, IClientOptions } from 'mqtt';
+import {toast} from "sonner";
 
-const MQTTComponent = () => {
+interface MqttComponentProps {
+    airport: string;
+    sensorList: string[];
+}
+
+function MqttComponent(props: MqttComponentProps) {
     const url = process.env.REACT_APP_MQTT_URL;
-    const clientId = process.env.REACT_APP_MQTT_CLIENT_ID;
-    const topic = process.env.REACT_APP_MQTT_TOPIC;
+    const username = process.env.REACT_APP_MQTT_USERNAME;
+    const password = process.env.REACT_APP_MQTT_PASSWORD;
 
-    if(url && clientId && topic) {
-        useMQTT(url, clientId, topic);
+    if(url && username && password) {
+        useEffect(() => {
+            const clientId = 'mqtt-client-' + Math.random().toString(16);
+
+            const options: IClientOptions = {
+                keepalive: 60,
+                clientId: clientId,
+                username: username,
+                password: password,
+                protocolId: 'MQTT',
+                protocolVersion: 5,
+                clean: true,
+                reconnectPeriod: 1000,
+                connectTimeout: 30 * 1000,
+            }
+            const client: MqttClient = mqtt.connect(url, options);
+
+            client.on('connect', () => {
+                console.log('MQTT client connected');
+                for (let sensor of props.sensorList) {
+                    client.subscribe("airport/" + props.airport + "/sensor/" +sensor + '/#');
+                }
+            });
+
+            client.on('message', (_topic: string, message: Buffer) => {
+                toast("Nouvelle alerte ", {
+                    description: 'Message : ' + message.toString(),
+                    action: {
+                        label: "Ok",
+                        onClick: () => console.log("Ok"),
+                    },
+                })
+            });
+
+            return () => {
+                client.end();
+            };
+        }, []);
+    }
+    else{
+        console.log("Missing environment variables for MQTT connection");
     }
 
-    return <div>Connexion MQTT en cours...</div>;
-};
+    return (
+        <div className="Mqtt"></div>
+    );
+}
 
-export default MQTTComponent;
+export default MqttComponent;
--- frontend-spa/vite.config.ts
+++ frontend-spa/vite.config.ts
@@ -1,13 +1,22 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import {defineConfig, loadEnv} from "vite"
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+    define: {
+      'process.env.REACT_APP_MQTT_URL': JSON.stringify(env.REACT_APP_MQTT_URL),
+      'process.env.REACT_APP_MQTT_PASSWORD': JSON.stringify(env.REACT_APP_MQTT_PASSWORD),
+      'process.env.REACT_APP_MQTT_USERNAME': JSON.stringify(env.REACT_APP_MQTT_USERNAME),
+    },
+  }
 })
 
