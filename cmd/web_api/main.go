89ecbaeca5bc8cc91a6c39a778a273f0d56ae676
karltokoekambi <acamer.guillaume@gmail.com>
2024-01-22 13:04:20 +0100

    finition swagger

--- cmd/web_api/main.go
+++ cmd/web_api/main.go
@@ -12,9 +12,11 @@ func main() {
 	r := mux.NewRouter()
 	controller := api.NewRestController()
 	r.HandleFunc("/dailyAverage", controller.DailyAverage).
-		Queries("day", "{day}")
+		Queries("day", "{day}").
+		Methods("GET")
 	r.HandleFunc("/onTimeList", controller.OnTimeList).
-		Queries("from", "{from}", "to", "{to}", "type", "{type}")
+		Queries("from", "{from}", "to", "{to}", "type", "{type}").
+		Methods("GET")
 
 	//Swagger
 	r.PathPrefix("/swagger/").Handler(httpSwagger.WrapHandler)
@@ -24,7 +26,6 @@ func main() {
 	// Démarrer le serveur sur le port 8080
 	port := 8080
 	fmt.Printf("Serveur écoutant sur le port %d...\n", port)
-	//http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	err := http.ListenAndServe(fmt.Sprintf(":%d", 8080), nil)
 	if err != nil {
 		fmt.Printf("Erreur : %v\n", err)
